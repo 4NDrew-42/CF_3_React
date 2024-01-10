66e21675211367f70e10c65c3539d19c0fa60e75
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2024-01-09 19:10:59 -0600

    CF-3_4_docs(readme): update README.md with integration prep and branching strategy for tutor review

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,58 +1,55 @@
-import { useState } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
+import { useState } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 import silenceImg from '../../img/silence.png';
 import akiraImg from '../../img/akira.png';
 import fountainImg from '../../img/fountain.png';
 
 export const MainView = () => {
-    const [movie, setMovie] = useState([
-    {
-      id: 1,
-      title: "Silence of the Lambs",
-      image: silenceImg,
-      description: "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
-      director: "Jonathan Demme"
-    },
-    {
-      id: 2,
-      title: "Akira",
-      image: akiraImg,
-      description: "A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath who can only be stopped by two teenagers and a group of psychics.",
-        director: "Katsuhiro Otomo"
-    },
-    {
-      id: 3,
-      title: "The Fountain",
-      image: fountainImg,
-      description: "As a modern-day scientist, Tommy is struggling with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.",
-        director: "Darren Aronofsky"
-    },
-  ]);
-        
-  
+	const [movie, setMovie] = useState([
+		{
+			id: 1,
+			title: 'Silence of the Lambs',
+			image: silenceImg,
+			description: 'A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.',
+			director: 'Jonathan Demme',
+		},
+		{
+			id: 2,
+			title: 'Akira',
+			image: akiraImg,
+			description:
+				'A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath who can only be stopped by two teenagers and a group of psychics.',
+			director: 'Katsuhiro Otomo',
+		},
+		{
+			id: 3,
+			title: 'The Fountain',
+			image: fountainImg,
+			description: 'As a modern-day scientist, Tommy is struggling with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.',
+			director: 'Darren Aronofsky',
+		},
+	]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+	const [selectedMovie, setSelectedMovie] = useState(null);
 
-    if (selectedMovie) {
-        return (
-        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
-    }
+	if (selectedMovie) {
+		return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+	}
 
-    if (movie.length === 0) return <div className="main-view">The list is empty!</div>;
+	if (movie.length === 0) return <div className="main-view">The list is empty!</div>;
 
-return (
-    <div>
-        {movie.map((movie) => (
-            <MovieCard
-            key={movie.id}
-            movie={movie} 
-            onMovieClick={(newSelectedMovie) => {
-                setSelectedMovie(newSelectedMovie);
-            }}
-            />
-            ))}
-    </div>
-    );
-}
+	return (
+		<div>
+			{movie.map((movie) => (
+				<MovieCard
+					key={movie.id}
+					movie={movie}
+					onMovieClick={(newSelectedMovie) => {
+						setSelectedMovie(newSelectedMovie);
+					}}
+				/>
+			))}
+		</div>
+	);
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import { createRoot } from 'react-dom/client';
 
-import { MainView } from "./components/main-view/main-view.jsx";
+import { MainView } from './components/main-view/main-view.jsx';
 
 // Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
+import './index.scss';
 
 // Main component (will eventually use all the others)
 const App = () => {
-  return <MainView />;
+	return <MainView />;
 };
 
 // Finds the root of your app
-const container = document.querySelector("#root");
+const container = document.querySelector('#root');
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
